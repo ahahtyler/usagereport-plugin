8179ba91eb9f0cfab48fccafb46c8d8403b823a6
Stigliano, Tyler <tyler.stigliano@one.verizon.com>
2015-11-20 10:39:38 -0500

    S&P Changes

--- apihelper/apihelper.go
+++ apihelper/apihelper.go
@@ -1,8 +1,6 @@
 package apihelper
 
 import (
-	"strconv"
-
 	"github.com/cloudfoundry/cli/plugin"
 	"github.com/krujos/cfcurl"
 )
@@ -26,8 +24,11 @@ type App struct {
 	Instances float64
 	RAM       float64
 	Running   bool
+	Name 	  string
+	Buildpack string
+	BuildpackDetected string
 }
-
+ 
 //CFAPIHelper to wrap cf curl results
 type CFAPIHelper interface {
 	GetOrgs(plugin.CliConnection) ([]Organization, error)
@@ -46,24 +47,19 @@ func (api *APIHelper) GetOrgs(cli plugin.CliConnection) ([]Organization, error) 
 	if nil != err {
 		return nil, err
 	}
-	pages := int(orgsJSON["total_pages"].(float64))
+
 	orgs := []Organization{}
-	for i := 1; i <= pages; i++ {
-		if 1 != i {
-			orgsJSON, err = cfcurl.Curl(cli, "/v2/organizations?page="+strconv.Itoa(i))
-		}
-		for _, o := range orgsJSON["resources"].([]interface{}) {
-			theOrg := o.(map[string]interface{})
-			entity := theOrg["entity"].(map[string]interface{})
-			metadata := theOrg["metadata"].(map[string]interface{})
-			orgs = append(orgs,
-				Organization{
-					Name:      entity["name"].(string),
-					URL:       metadata["url"].(string),
-					QuotaURL:  entity["quota_definition_url"].(string),
-					SpacesURL: entity["spaces_url"].(string),
-				})
-		}
+	for _, o := range orgsJSON["resources"].([]interface{}) {
+		theOrg := o.(map[string]interface{})
+		entity := theOrg["entity"].(map[string]interface{})
+		metadata := theOrg["metadata"].(map[string]interface{})
+		orgs = append(orgs,
+			Organization{
+				Name:      entity["name"].(string),
+				URL:       metadata["url"].(string),
+				QuotaURL:  entity["quota_definition_url"].(string),
+				SpacesURL: entity["spaces_url"].(string),
+			})
 	}
 	return orgs, nil
 }
@@ -111,15 +107,35 @@ func (api *APIHelper) GetSpaceApps(cli plugin.CliConnection, appsURL string) ([]
 	if nil != err {
 		return nil, err
 	}
+	
 	apps := []App{}
 	for _, a := range appsJSON["resources"].([]interface{}) {
 		theApp := a.(map[string]interface{})
 		entity := theApp["entity"].(map[string]interface{})
+		
+		bp := " "
+		bpd := " "
+		
+		if entity["buildpack"] == nil{
+			bp = "Not detected"
+		} else {
+			bp = entity["buildpack"].(string)
+		}
+		
+		if entity["detected_buildpack"] == nil {
+			bpd = "Not detected"
+		} else {
+			bpd = entity["detected_buildpack"].(string)			
+		}
+		
 		apps = append(apps,
 			App{
 				Instances: entity["instances"].(float64),
 				RAM:       entity["memory"].(float64),
 				Running:   "STARTED" == entity["state"].(string),
+				Name: 		entity["name"].(string),
+				Buildpack:  bp,
+				BuildpackDetected: bpd,
 			})
 	}
 	return apps, nil
